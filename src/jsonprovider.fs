d792c69c5b34009f81d6e91fac51e3a3b709097f
Sebs <72739852+sebsthiel@users.noreply.github.com>
2021-05-12 11:52:38 +0200

    Changes to Schema and jsonprovider.Data  because of test data. Moving over to ConfigurationList

--- src/jsonprovider.fs
+++ src/jsonprovider.fs
@@ -5,49 +5,46 @@ open Microsoft.FSharp.Linq.NullableOperators
 module data =
 
    type Data = FSharp.Data.JsonProvider<"""[{
-       "Project Name" : "lkjlkj",
-       "TimeStamp": "03/11/2021 10:34:15",
-       "Sprint Name": null,
-       "WorkItemId": 79312,
-       "ChangedDate": "04/30/2019 14:57:50",
-       "WorkItemType": "User Story",
-       "CreatedDate": "02/27/2019 12:27:11",
-       "ClosedDate": "04/30/2019 14:57:50",
-       "LeadTimeDays": "62.104618",
-       "CycleTimeDays": "27.2778819",
-       "StoryPoints": null,
-       "RevisedDate": "01/01/9999 00:00:00",
-       "Priority": 2,
-       "Title": "Cake celebration at this point",
-       "Sprint Number": null,
-       "State": "Done"
-     },{
-       "Project Name" : "lkjlkj",
-       "TimeStamp": "03/11/2021 10:34:15",
-       "Sprint Name": "Iteration 3",
-       "WorkItemId": 77520,
-       "ChangedDate": "03/27/2019 14:42:54",
-       "WorkItemType": "User Story",
-       "CreatedDate": "02/13/2019 08:31:15",
-       "ClosedDate": "03/27/2019 14:42:54",
-       "LeadTimeDays": "42.2580902",
-       "CycleTimeDays": "0.0",
-       "StoryPoints": 3,
-       "RevisedDate": "01/01/9999 00:00:00",
-       "Priority": 2,
-       "Title": "user specific dashboards",
-       "Sprint Number": 3,
-       "State": "Done"
-     }]""">
+  "Sprint Name": null,
+  "WorkItemId": 442401,
+  "ChangedDate": "11/19/2020 07:41:51",
+  "WorkItemType": "User Story",
+  "CreatedDate": "09/21/2020 18:50:11",
+  "ClosedDate": "11/19/2020 07:41:51",
+  "LeadTimeDays": "58.5358796",
+  "CycleTimeDays": "3.47E-05",
+  "StoryPoints": 2,
+  "RevisedDate": "01/01/9999 00:00:00",
+  "Title": "Web UI for visualiser",
+  "State": "Done",
+  "Sprint Number" : null
+}, {
+    "TimeStamp": "03/11/2021 10:34:15",
+    "Sprint Name": "null",
+    "WorkItemId": 79312,
+    "ChangedDate": "04/30/2019 14:57:50",
+    "WorkItemType": "User Story",
+    "CreatedDate": "02/27/2019 12:27:11",
+    "ClosedDate": "04/30/2019 14:57:50",
+    "LeadTimeDays": "62.104618",
+    "CycleTimeDays": "27.2778819",
+    "StoryPoints": null,
+    "RevisedDate": "01/01/9999 00:00:00",
+    "Priority": 2,
+    "Title": "Cake celebration at this point",
+    "Sprint Number": 5,
+    "State": "Done"
+  }]""" >
 
+   // OLD type Structure
    type Sprint = {
       SprintNumber  : int option 
-      ProjectName : string
+      SprintName : string
       WorkItems : Data.Root seq
    }
 
    type Project = {
-      ProjectName : string
+      SprintName : string
       Sprints : Sprint seq
    }
 
@@ -86,9 +83,9 @@ module data =
                 },
             "transformations" : ["jlk","lkjlk"]
         }]""">
-   (*
+   
    let configurationlist() = 
-      ConfigList.Load "http://configurations-svc:8085/configurationlist"     
+      ConfigList.Load "http://configurations-svc:8085/meta/category/workitems"     
       |> Array.collect(fun config -> 
          let key = config.Id
          try 
@@ -99,13 +96,13 @@ module data =
             eprintfn"Exeption when calling uniformdata, defaulting to sampledata %s %s" e.Message e.StackTrace
             Data.GetSamples()
       )
-   *)
+   
    let datasamples = Data.GetSamples()
 
    let projectMap() = 
-      datasamples
-      //configurationlist()
-      |> Seq.groupBy(fun p -> p.ProjectName)
+      //datasamples
+      configurationlist()
+      |> Seq.groupBy(fun p -> p.SprintName)
       |> Seq.map(fun (pn,record) -> 
          pn,record 
             |> Seq.groupBy(fun project -> project.SprintNumber)
@@ -113,24 +110,40 @@ module data =
          |> Map.ofSeq  
 
    let typeProjectMap() =
-      datasamples
-      |> Seq.groupBy(fun p -> p.ProjectName)
-      |> Seq.map(fun (pn,sp) -> {
-         ProjectName = pn
-         Sprints = ( sp
-            |> Seq.groupBy(fun spn -> spn.SprintNumber)
-            |> Seq.map(fun (spnmbr,wi) -> {
-               SprintNumber = spnmbr
-               ProjectName = pn
-               WorkItems = wi
-            })
-         )
-      })
+      //datasamples
+      configurationlist()
+      |> Seq.groupBy(fun p -> p.SprintName)
+      |> Seq.map(fun (pn,sp) ->
+         match pn with
+         |None -> 
+            {
+               SprintName = null
+               Sprints = ( sp
+                  |> Seq.groupBy(fun spn -> spn.SprintNumber)
+                  |> Seq.map(fun (spnmbr,wi) -> {
+                     SprintNumber = spnmbr
+                     SprintName = null
+                     WorkItems = wi
+                  })
+               )    
+            }
+         |Some x ->
+          {
+            SprintName = x
+            Sprints = ( sp
+               |> Seq.groupBy(fun spn -> spn.SprintNumber)
+               |> Seq.map(fun (spnmbr,wi) -> {
+                  SprintNumber = spnmbr
+                  SprintName = x
+                  WorkItems = wi
+               })
+            )    
+         })
    
    let typeDataHierarchy() = 
       let projectMap = projectMap()
-      //configurationlist()
-      datasamples
+      configurationlist()
+      //datasamples
       |> Array.groupBy(fun sp -> sp.SprintNumber)
 
       // Type SprintLayer is assigned a SprintLayerNumber
@@ -143,23 +156,40 @@ module data =
          SprintLayerNumber = sn
          Projects = 
             (records
-            |> Seq.map(fun record -> record.ProjectName)
+            |> Seq.map(fun record -> record.SprintName)
             |> Set.ofSeq
             // Type Project gets assigned with a ProjectName and a 
             // Sequence of associated Sprints.
             |> Seq.map(fun pn -> 
-               assert(pn.Length > 0)
-               {
-                  ProjectName = pn
-                  Sprints = 
-                     ( 
-                     match projectMap |> Map.tryFind pn with
-                     | Some sp -> 
-                        sp |>  Seq.map(fun (sp,wi) ->
-                           {SprintNumber = sp
-                            ProjectName = pn
-                            WorkItems = wi})
-                     | None -> Seq.empty     
-                     )
-               }))
+               //assert(pn.Length > 0)
+               match pn with
+               |None ->
+                  {
+                     SprintName = null
+                     Sprints = 
+                        ( 
+                        match projectMap |> Map.tryFind pn with
+                        | Some sp -> 
+                           sp |>  Seq.map(fun (sp,wi) ->
+                              {SprintNumber = sp
+                               SprintName = null
+                               WorkItems = wi})
+                        | None -> Seq.empty     
+                        )
+                  }
+               |Some x ->
+                  {
+                     SprintName = x
+                     Sprints = 
+                        ( 
+                        match projectMap |> Map.tryFind pn with
+                        | Some sp -> 
+                           sp |>  Seq.map(fun (sp,wi) ->
+                              {SprintNumber = sp
+                               SprintName = x
+                               WorkItems = wi})
+                        | None -> Seq.empty     
+                        )
+                  }
+                )) 
             })
